93f91b648d33c4883d51b69499dbada5dd61ba9d
vincent.mary <vincent.mary@ovo.com>
2025-03-21 16:08:50 +0000

    fix (lazyvim): Svelte plugin import path and add lazy-lock and lazyvim configuration files

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.helm" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "tlazyvim.plugins.extras.lang.svelte" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
